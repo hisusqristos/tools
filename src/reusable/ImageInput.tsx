5d358dc8bac53386e3af10efde2f91ce90264633
hisusqristos <varyadaser@gmail.com>
2025-03-31 17:23:14 +0400

    :feet: intuitive uploading

--- src/reusable/ImageInput.tsx
+++ src/reusable/ImageInput.tsx
@@ -6,27 +6,47 @@ interface ImageInputProps {
 
 const ImageInput: React.FC<ImageInputProps> = ({ onUploadAction }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
+  const [isDragging, setIsDragging] = React.useState(false);
 
   const handleContainerClick = () => {
-    if (inputRef.current) {
-      inputRef.current.click();
+    inputRef.current?.click();
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(false);
+
+    if (event.dataTransfer.files.length > 0) {
+      onUploadAction({ target: { files: event.dataTransfer.files } } as React.ChangeEvent<HTMLInputElement>);
     }
   };
 
   return (
-    <div 
-      className="flex flex-row items-center justify-center px-40 py-24  border-2 border-dashed border-purple-500 rounded-lg bg-beige cursor-pointer hover:border-purple-700 transition-colors"
+    <div
+      className={`flex flex-row items-center justify-center px-40 py-24 border-2 border-dashed rounded-lg bg-beige-200 cursor-pointer hover:bg-beige-300 transition-colors ${isDragging ? "border-purple-700 bg-beige-300" : "border-purple-500 hover:border-purple-700"}`}
       onClick={handleContainerClick}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
     >
       <div className="w-5 h-5 mx-1 text-gray-600 gap-1">
         <img src="assets/cloud-upload.svg" alt="Upload" />
       </div>
-      <h6 className="text-sm font-medium text-gray-600">Upload Image</h6>
-      <input 
+      <h6 className="text-sm font-medium text-gray-600"> Upload Image </h6>
+      <input
         ref={inputRef}
-        type="file" 
-        accept="image/*" 
-        onChange={onUploadAction} 
+        type="file"
+        accept="image/*"
+        onChange={onUploadAction}
         className="sr-only" // Tailwind's screen reader only class
       />
     </div>
