859925ed8dae8cbb8661404d39718b4c7cc7d20a
hisusqristos <varyadaser@gmail.com>
2025-04-01 09:28:49 +0400

    :axe: downloadButton to separate file

--- src/reusable/DownloadButton.tsx
+++ src/reusable/DownloadButton.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 
 interface DownloadButtonProps {
-  onClickAction: () => void;
+  downloadAction?: () => void;
   disabled?: boolean;
 }
 
-const DownloadButton: React.FC<DownloadButtonProps> = ({ 
-  onClickAction, 
-  disabled = false 
+const DownloadButton: React.FC<DownloadButtonProps> = ({
+  downloadAction,
+  disabled = false
 }) => {
   return (
-    <button 
-      className={`flex items-center space-x-2 px-4 py-2 rounded-md border ${disabled ? 'border-gray-200 text-gray-400 cursor-not-allowed' : 'border-gray-200 text-purple-600 hover:border-purple-600 cursor-pointer'}`}
-      onClick={onClickAction} 
-      disabled={disabled}
+    <button
+      className={`flex items-center space-x-2 px-3 py-1 rounded-md border ${downloadAction ? 'border-purple-600  text-purple-600' : 'border-gray-300 text-gray-300'}`}
+      onClick={downloadAction}
+      disabled={!downloadAction}
     >
-      <span className="w-5 h-5">
-        <img src="assets/download.svg" alt="Download" className="w-full h-full" />
-      </span>
-      <span className="font-medium">Download</span>
+      <img className="size-3 text-white" src="assets/download.svg" alt="Download" />
+      <span className="text-small font-sans">Download</span>
     </button>
   );
 };
