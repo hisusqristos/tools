6d4a3b0aa24a33c343938c06ec9dd31c188cc7fa
hisusqristos <varyadaser@gmail.com>
2025-04-01 12:28:39 +0400

    :broom: remove leftover /unused/ declarations

--- src/tools/crop/Crop.tsx
+++ src/tools/crop/Crop.tsx
@@ -1,13 +1,11 @@
 import { useRef } from "react"
-import DragAndDrop from "../../reusable/DragAndDrop"
-import DownloadButton from "../../reusable/DownloadButton"
 import useHandleFile from "../../hooks/useHandleFile";
 import UploadButton from "../../reusable/UploadButton";
 
 const Crop = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const overlayCanvasRef = useRef<HTMLCanvasElement | null>(null);
-    const { image, handleUpload, handleDownload } = useHandleFile(canvasRef);
+    const { image, handleUpload } = useHandleFile(canvasRef);
 
     return (<>
         <UploadButton onUpload={handleUpload} />
